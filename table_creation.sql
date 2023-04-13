c2d881f750faccc2305afd349568fbf33b8abd36
Katarya Johnson-Williams <71856013+HoneyKat11@users.noreply.github.com>
2023-04-13 13:17:02 -0400

    Added pre-existing files

---
+++ table_creation.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Ambassador (
+	UIN INT PRIMARY KEY,
+   	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	major CHAR(50),
+	is_ambassador INTEGER
+);
+
+CREATE TABLE Participated (
+	UIN INT,
+   	position_ID INT,
+	hour_modifier INT DEFAULT 0,
+	PRIMARY KEY (UIN, position_ID),
+	FOREIGN KEY (UIN)
+		REFERENCES Ambassador (UIN)
+			ON UPDATE CASCADE
+	FOREIGN KEY (position_ID)
+		REFERENCES Position (position_ID)
+			ON UPDATE CASCADE
+);
+
+CREATE TABLE Position (
+	event_ID INT,
+   	position_ID INT,
+	pos_name TEXT NOT NULL,
+	base_hours REAL,
+	PRIMARY KEY (event_ID, position_ID),
+	FOREIGN KEY (event_ID)
+		REFERENCES Event (event_ID)
+			ON UPDATE CASCADE
+);
+
+CREATE TABLE Event (
+	event_ID INT PRIMARY KEY,
+   	name TEXT NOT NULL,
+	date TEXT,
+	academic_year TEXT
+);
